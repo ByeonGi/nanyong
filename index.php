c28d6edea0d342d992473d6970f347fba276d9c5
ByeonGi <ghgh2090@gmail.com>
2019-12-22 14:30:36 +0900

    git

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+  include_once "./mission/index.php";
+  // 영이 미션
+?>
--- mission/index.php
+++ mission/index.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="./index.css" type = "text/css">
+    <link rel="stylesheet" href="./mission/index.css" type = "text/css">
     <title>Document</title>
+    <script>
+        <?php echo "1"?>
+    </script>
 </head>
 <body>
     <div class ="wrap">
